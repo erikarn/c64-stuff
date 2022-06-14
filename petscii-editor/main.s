3f2b6d1c2eafd7192a05f2c47c872de91f6d34f2
Adrian Chadd <adrian.chadd@fb.com>
2022-06-13 20:55:06 -0700

    [petscii-editor] initial test of the petscii editor hacks

--- petscii-editor/main.s
+++ petscii-editor/main.s
@@ -1,7 +1,22 @@
 
+; start of screen memory - $0400 - $07e7
+; start of basic area - $0800 -> $9fff
+; color ram - $d800 -> $dbff
+
+.define scinit $ff81
+.define ioinit $ff84
+.define chrin $ffcf
+.define chrout $ffd2
 
 Start:
+	; Initialise the system defaults
+	jsr scinit
+	jsr ioinit
+
+	; For now, just loop over and echo back to the
+	; screen device what we input.
 loop:
-	inc $d021
+	jsr chrin
+	jsr chrout
 	jmp loop
 
