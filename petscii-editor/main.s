1701d28e975d51e9b8b46218c56bee849275c561
Adrian Chadd <adrian.chadd@fb.com>
2022-06-13 22:22:04 -0700

    [petscii-editor] more hackery, it's starting up!

--- petscii-editor/main.s
+++ petscii-editor/main.s
@@ -6,7 +6,11 @@
 .define scinit $ff81
 .define ioinit $ff84
 .define chrin $ffcf
+.define getin $ffe4
 .define chrout $ffd2
+
+.define vic_background $d021
+.define vic_border $d020
 
 Start:
 	; Initialise the system defaults
@@ -15,8 +19,73 @@ Start:
 
 	; For now, just loop over and echo back to the
 	; screen device what we input.
-loop:
-	jsr chrin
+
+app_loop:
+	; read a character
+	jsr getin
+
+	; let's handle the key input filtering here; anything
+	; else can just go straight to display_and_loop
+
+	; cursor key - don't allow scrolling up or down, make
+	; sure our window stays fixed!
+
+	; f1 - save
+	cmp #$85
+
+	; f2 - load
+	cmp #$89
+
+	; f3
+	cmp #$86
+
+	; f4
+	cmp #$8a
+
+	; f5 - cycle background
+	cmp #$87
+	beq menu_f5
+
+	; f6 - cycle border
+	cmp #$8b
+	beq menu_f6
+
+	; f7
+	cmp #$88
+
+	; f8
+	cmp #$8c
+
+display_and_loop:
 	jsr chrout
-	jmp loop
+	jmp app_loop
+
+menu_f5:
+	jsr cycle_background
+	jmp app_loop
+
+menu_f6:
+	jsr cycle_border
+	jmp app_loop
+
+cycle_background:
+	lda background_color
+	adc #$01
+	and #$0f
+	sta background_color
+	sta vic_background
+	rts
+
+cycle_border:
+	lda border_color
+	adc #$01
+	and #$0f
+	sta border_color
+	sta vic_border
+	rts
+
+border_color:
+.byte 14
+background_color:
+.byte 6
 
